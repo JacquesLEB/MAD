984c3f9e874e2eb9a40e55eefd28c591235e3071
Jacques Le Breuilly <Jacques Le Breuilly>
2017-02-08 13:43:41 +0000

    Initial map activity

---
+++ app/src/main/java/com/example/a1lebrj48/mapping/MapChooseActivity.java
@@ -0,0 +1,16 @@
+package com.example.a1lebrj48.mapping;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class MapChooseActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mca);
+    }
+}
